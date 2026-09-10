6ddab60c814de0a4031597a29c691df85880082c
leanty <134470300+leanty@users.noreply.github.com>
2026-09-11 07:21:26 +0800

    Update astro-paper.config.ts

--- astro-paper.config.ts
+++ astro-paper.config.ts
@@ -2,7 +2,7 @@ import { defineAstroPaperConfig } from "./src/types/config";
 
 export default defineAstroPaperConfig({
   site: {
-    url: "https://astro-paper.pages.dev/",
+    url: "https://leanty.github.io/astropaper",
     title: "AstroPaper",
     description: "A minimal, responsive and SEO-friendly Astro blog theme.",
     author: "Sat Naing",
